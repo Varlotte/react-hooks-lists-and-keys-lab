d8eff33fd22c5d9d504fe883f07e037358b58e5a
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-25 15:58:49 -0400

    .maps through array of links to fix nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const mappedLinks = links.map((link) => {
+    return (
+      <a href={`#${link}`} key={link}>
+        {link}
+      </a>
+    );
+  });
+  return <nav>{mappedLinks}</nav>;
 }
 
 export default NavBar;
